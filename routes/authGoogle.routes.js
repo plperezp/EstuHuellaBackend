09f0155f45a3481942a21197028a8f3636ae0e07
Pedro Perez <plperezpiris@gmail.com>
2024-10-21 11:42:38 +0200

    passport

---
+++ routes/authGoogle.routes.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const User = require('../models/User.model')
+const passport = require('passport')
+const router = express.Router()
+const mongoose = require('mongoose')
+
+router.get(
+  '/',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+)
+
+router.get(
+  '/callback',
+  passport.authenticate(google, { failureRedirect: '/login' }),
+  async (req, res) => {
+    try {
+      const existingUser = await User.findOne({ googleId: req.user.id })
+
+      if (existingUser) {
+        req.logIn(existingUser, (err) => {
+          if (err) return next(err)
+          res.redirect('/')
+        })
+      } else {
+        //si no existe crea el usuario
+
+        const newUser = new User({
+          googleId: req.user.id,
+          email,
+          name,
+          password,
+          huella,
+          mediHuella,
+          img,
+          username,
+        })
+        await newUser.create()
+        req.login(newUser, (err) => {
+          if (err) return next(err)
+          res.redirect('/')
+        })
+      }
+    } catch (error) {}
+  }
+)
+module.exports = router
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,20 +1,22 @@
-const router = require("express").Router();
+const router = require('express').Router()
 
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
-});
+router.get('/', (req, res, next) => {
+  res.json('All good in here')
+})
 
-const authRouter = require("./auth.routes")
-router.use("/auth", authRouter)
+const authRouter = require('./auth.routes')
+router.use('/auth', authRouter)
 
+const authRouter = require('./authGoogle.routes')
+router.use('/auth/google', authRouter)
 
-const habitoRouter = require ("./habito.routes")
-router.use("/huella",habitoRouter)
+const habitoRouter = require('./habito.routes')
+router.use('/huella', habitoRouter)
 
-const postRouter = require("./post.routes")
-router.use("/foro", postRouter)
+const postRouter = require('./post.routes')
+router.use('/foro', postRouter)
 
-const userRouter =require("./user.routes")
-router.use("/user",userRouter)
+const userRouter = require('./user.routes')
+router.use('/user', userRouter)
 
-module.exports = router;
+module.exports = router
