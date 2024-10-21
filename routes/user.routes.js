3df20215519885fbee90b8978aeae7aba3a76ec7
Javier Gascon <javitocatral@gmail.com>
2024-10-21 15:50:26 +0200

    calualhuella

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,6 @@
 const router = require('express').Router()
 const verifyToken = require('../middlewares/auth.middlewares')
 const User = require('../models/User.model')
-const calculahuella = require('../utils/Calculahuella')
-
-const { huellaDiaria } = calculahuella
 
 router.get('/', verifyToken, async (req, res, next) => {
   try {
@@ -26,18 +23,6 @@ router.patch('/', verifyToken, async (req, res, next) => {
     )
 
     res.status(200).json(response)
-  } catch (error) {
-    next(error)
-  }
-})
-
-router.patch('/huella', verifyToken, async (req, res, next) => {
-  try {
-    const response = await User.findByIdAndUpdate(
-      req.payload._id,
-      { $push: { huella: { $each: [huellaDiaria], $position: 0 } } },
-      { new: true }
-    )
     res.status(200).json(response)
   } catch (error) {
     next(error)
