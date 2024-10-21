e7f235ed6e92bbc43eff1482c75108fce63b47ec
Javier Gascon <javitocatral@gmail.com>
2024-10-21 15:53:16 +0200

    pach

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -23,6 +23,20 @@ router.patch('/', verifyToken, async (req, res, next) => {
     )
 
     res.status(200).json(response)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.patch('/huella', verifyToken, async (req, res, next) => {
+  const { huella } = req.body
+
+  try {
+    const response = await User.findByIdAndUpdate(
+      req.payload._id,
+      { $push: { huella: { $each: [huella], $position: 0 } } },
+      { new: true }
+    )
     res.status(200).json(response)
   } catch (error) {
     next(error)
