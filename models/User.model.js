f92eccefba464c5ed4b0b6d5b9aa4a7412225f4e
Pedro Perez <plperezpiris@gmail.com>
2024-10-24 19:30:36 +0200

    done

--- models/User.model.js
+++ models/User.model.js
@@ -1,54 +1,50 @@
-const { Schema, model } = require("mongoose");
-const mongoose = require("mongoose");
-
-
+const { Schema, model } = require('mongoose')
+const mongoose = require('mongoose')
 
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new mongoose.Schema(
   {
-
     username: {
-      type: String, 
-      required: [true, "Usermane is required"],
-      unique: true
+      type: String,
+      required: [true, 'Usermane is required'],
+      unique: true,
     },
 
     name: {
-      type:String,
-      required: [true, "Name is required"]
+      type: String,
+      required: [true, 'Name is required'],
     },
     email: {
       type: String,
       required: [true, 'Email is required.'],
       unique: true,
       lowercase: true,
-      trim: true
+      trim: true,
     },
     password: {
       type: String,
-      required: [true, 'Password is required.']
+      required: [true, 'Password is required.'],
     },
 
-    huella:{
-      type:[Number]
+    huella: {
+      type: [Number],
     },
-    mediHuella:{
+    mediHuella: {
       type: Number,
       min: 0,
-      default: 0
+      default: 0,
     },
-    img:{
-      type:String
-    }
+    img: {
+      type: String,
+      default: '../public/default.png',
+    },
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`    
-    timestamps: true
-
-
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
   }
-);
+)
 
-const User = model("User", userSchema);
+const User = model('User', userSchema)
 
-module.exports = User;
+module.exports = User
