57841284a954fd9f9a41636952dad244fbb2dd03
Pedro Perez <plperezpiris@gmail.com>
2024-10-24 10:03:28 +0200

    done

--- routes/publicUser.routes.js
+++ routes/publicUser.routes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router()
 const User = require('../models/User.model')
 
-router.get('/user', async (req, res) => {
+router.get('/user', async (req, res, next) => {
   try {
-    const response = await User.find().select('username', 'img')
+    const response = await User.find().select('username img')
     return res.status(200).json(response)
   } catch (error) {
     next(error)
